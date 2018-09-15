33716b55a5d4ac19f3f5bcf7523662697c2e498a
TXY <532868890@qq.com>
2018-09-15 10:04:49 +0800

    数据库配置

---
+++ model/DAO.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+const config = require("../model/dbconfig");
+const pool = mysql.createPool(config);
+function query(sql, values) {
+    return new Promise((resolve, reject) => {
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                reject(err);
+            } else {
+                connection.query(sql, values, (err, rows) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(rows);
+                    }
+                    connection.release();
+                })
+            }
+        })
+    })
+}
+module.exports = query;
---
+++ model/dbconfig.js
@@ -0,0 +1,8 @@
+const config = {
+    host: "localhost",
+    post: 3306,
+    user: "root",
+    password: "123456",
+    database: "kaka"
+};
+module.exports = config;
