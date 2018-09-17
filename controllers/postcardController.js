ebfb1a2bd902d7f5841119361464db6456c5ce6a
Joan Yu <eoin@live.in>
2018-09-17 16:52:17 +0800

    加入评论区的内容

--- controllers/postcardController.js
+++ controllers/postcardController.js
@@ -1,7 +1,7 @@
 const postcardDAO = require('../model/postcardDAO');
 module.exports = {
     //index--明信片实时动态-收到最新动态
-    getNewReceive:async (ctx, next) => {
+    getNewReceive:async (ctx, next) =>{
         try {
             let newRecevie= await postcardDAO.getNewReceive();
             ctx.body = {"code": 200, "message": "ok:最新实时收到动态", data: newRecevie};
@@ -33,13 +33,42 @@ module.exports = {
         }
     },
 
+    //wall--查询所有的明信片
+    getAllCard:async (ctx,next)=>{
+        try{
+            let all=await postcardDAO.getAllCard();
+            console.log(all)
+            ctx.body = {'code': 200, "message": "ok", data: all};
+        }catch (e){
+            ctx.body = {'code': 500, "message": "没有查到明信片！！", data:[]};
+        }
 
+    },
     //wall--根据输入的省份查询明信片
     getPostCard:async (ctx,next)=> {
         console.log(ctx.params.city)
         // console.log(ctx.query)
-        let jsondata = await postcardDAO.getPostCard(ctx.params.city);
-        ctx.body = {'code': 200, "message": "ok", data: jsondata};
+        try{
+            let jsondata = await postcardDAO.getPostCard(ctx.params.city);
+            ctx.body = {'code': 200, "message": "ok", data: jsondata};
+        }catch (e){
+            ctx.body = {'code': 500, "message": "服务器错误！", data:[]};
+        }
+    },
+    //postcard--点击照片墙上的照，显示明信片详情界面
+    getCardInformation:async (ctx,next)=>{
+        try{
+            console.log(ctx.params.cardId);
+            let cardInformation=await postcardDAO.getCardInformation(ctx.params.cardId);
+            let cardComment=await postcardDAO.getComment(ctx.params.cardId);
+            let content = {
+                cardInformation:cardInformation,
+                cardComment:cardComment
+            };
+            ctx.body = {'code': 200, "message": "ok", data: content};
+        }catch (e){
+            ctx.body = {'code': 500, "message": "点击明信片没查到信息！", data:[]};
+        }
     }
 
 };
