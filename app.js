31bb163f8e92670b25038789d138002c32ee4564
Joan Yu <eoin@live.in>
2018-09-17 11:13:43 +0800

    测试上传

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const logger = require('koa-logger')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
+const wall = require('./routes/wall')
 
 // error handler
 onerror(app)
@@ -35,6 +36,7 @@ app.use(async (ctx, next) => {
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(wall.routes(), wall.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
