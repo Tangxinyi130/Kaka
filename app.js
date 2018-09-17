7363ee9ce537cc94c5bce4d58fe39405252e5f9b
Gaoyue <2602121448@qq.com>
2018-09-17 14:35:36 +0800

    pool第二次提交，修改了app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const logger = require('koa-logger')
 const index = require('./routes/index')
 const users = require('./routes/users')
 const wall = require('./routes/wall')
+const send = require('./routes/send')
 
 // error handler
 onerror(app)
@@ -37,6 +38,7 @@ app.use(async (ctx, next) => {
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
 app.use(wall.routes(), wall.allowedMethods())
+app.use(send.routes(), send.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
