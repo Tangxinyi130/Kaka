d2d7fbbb5dda77a3557a405cea257e77d1c589c8
Joan Yu <eoin@live.in>
2018-09-18 16:19:01 +0800

    activity页面加载和点击活动进入活动详情界面

--- routes/activity.js
+++ routes/activity.js
@@ -1,4 +1,19 @@
 const router = require('koa-router')()
+const activityController=require('../controllers/activityController');
 router.prefix('/activity')
 
+//获取活动界面信息
+router.get('/', async(ctx, next)=>{
+    await activityController.getAllActivity(ctx,next);
+});
+
+//点击每个主题活动
+router.get('/topic/:activityId', async(ctx, next)=>{
+    await  activityController.getActivity(ctx,next);
+});
+
+//点击每个主题活动
+router.get('/address/:activityId', async(ctx, next)=>{
+    await  activityController.getActivity(ctx,next);
+});
 module.exports = router;
