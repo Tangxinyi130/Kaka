40c195fcd1558d3eac64096e20edae9eec3abdbe
Joan Yu <eoin@live.in>
2018-10-28 15:34:09 +0800

    收藏明信片

--- routes/postcards.js
+++ routes/postcards.js
@@ -15,6 +15,10 @@ router.post('/addcomment', async(ctx, next)=>{
 router.get('/collection/:cardId', async(ctx, next)=>{
     await collectionController.getCollectionNumber(ctx,next);
 });
+//postcard查询明信片是否存在收藏表里
+router.get('/getcollection/:cardId/:userId', async(ctx, next)=>{
+    await collectionController.checkCollection(ctx,next);
+});
 //用户关注，点赞数加1
 router.get('/like/:cardId', async(ctx, next)=>{
     await postcardController.addLike(ctx,next);
@@ -25,11 +29,11 @@ router.get('/unlike/:cardId', async(ctx, next)=>{
 });
 
 //收藏明信片，插入收藏用户id和卡片id
-router.get('/collect/:cardId', async(ctx, next)=>{
+router.get('/collect/:cardId/:userId', async(ctx, next)=>{
     await collectionController.insertCollection(ctx,next);
 });
 //收藏明信片，删除收藏用户id和卡片id
-router.get('/uncollect/:cardId', async(ctx, next)=>{
+router.get('/uncollect/:cardId/:userId', async(ctx, next)=>{
     await collectionController.deleteCollection(ctx,next);
 });
 
